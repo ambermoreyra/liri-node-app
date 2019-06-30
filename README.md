3cf82321f5ed0223e448cf4e4ff677d56e93970d
Kona Marie <ambermoreyra@gmail.com>
2019-06-30 11:17:41 -0600

    do-what-it-says complete and ReadMe partial complete

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # liri-node-app
+
+In this CLI app, the user is able to obtain more information about movies, songs, and upcoming concerts.
+
+Overall, there are four sections:
+    1. Search the spotify-node-api for songs using "spotify-this-song" as the first argument and the song title as the second argument. This will output the following information.
+        1. Artist name
+        2. Song name
+        3. Spotify preview link
+        4. Album name
+
+        ![Spotify example screenshot](/img/spotify_example.png)
+        
+    2. Search OMDB api for movies using "movie-this" as the first argument and the movie title as the second argument. This will output the following information.
+        1. Movie title
+        2. Year
+        3. IMDB rating
+        4. Rotten Tomatoes rating
+        5. Country
+        6. Language
+        7. Plot
+
+        ![OMDB example screenshot](/img/omdb_example.png)
+
+    3. Search Bands in Town api for concerts using "concert-this" as the first argument and the band/artist name as the second argument. This will output the following information for every upcoming event.
+        1. Venue name
+        2. Location
+        3. Date
+
+        ![Bands In Town example screenshot](/img/bands_in_town_example.png)
+
+    4. The user can also input any of these search terms into the log.txt file separating the first and second arguments with a comma. The user will then enter "do-what-it-says" into the command line. This will output one of the first three options.
+
+        ![do-what-it-says example screenshot](/img/do_what_it_says_example.png)
--- liri.js
+++ liri.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const axios = require("axios");
 var Spotify = require("node-spotify-api");
 var moment = require("moment");
+var fs = require("fs");
 
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
@@ -12,56 +13,85 @@ var searchMovie = process.argv.slice(3).join("+");
 var searchConcert = process.argv.slice(3).join("%20");
 var display = process.argv.slice(3).join(" ");
 var searchTerm = process.argv.slice(3);
+var seperate = "=================================";
 
 function spotifySearch() {
-    console.log("This is the spotify search");
-    if(process.argv.length === 3){
+    console.log(`\n\nSPOTIFY RESULTS FOR: ${display}\n${seperate}\n`);
+    if (process.argv[3] === "") {
         searchTerm = "Closer to Fine";
     }
     spotify.search({
         type: "track",
         query: searchTerm,
         limit: 3,
-    }, function(err,data) {
-        if(err) {
+    }, function (err, data) {
+        if (err) {
             return console.log("Error occurred: " + err);
         }
         for (var i = 0; i < data.tracks.items.length; i++) {
 
-        console.log(`Artist: ${data.tracks.items[i].artists[0].name}\nSong Name: ${data.tracks.items[i].name}\nSpotify Preview Link: ${data.tracks.items[i].preview_url}\nAlbum: ${data.tracks.items[i].album.name}\n\n`);
-    }
+            console.log(`--------------------\nArtist: ${data.tracks.items[i].artists[0].name}\nSong Name: ${data.tracks.items[i].name}\nSpotify Preview Link: ${data.tracks.items[i].preview_url}\nAlbum: ${data.tracks.items[i].album.name}\n--------------------\n\n`);
+        }
     })
 }
 
 function omdbSearch() {
-    
-    if(process.argv.length === 3){
+
+    if (process.argv[3] === "") {
         searchMovie = "Mean Girls";
     }
+    console.log(`\n\nOMDB RESULTS FOR: ${display}\n${seperate}\n`);
 
     axios.get("http://www.omdbapi.com/?apikey=1557ce71&t=" + searchMovie)
         .then(function (response) {
-        
-            console.log(`Title: ${response.data.Title}\nYear: ${response.data.Year}\nIMDB Rating: ${response.data.imdbRating}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.data.Language}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}`);
-
+            console.log(`Title: ${response.data.Title}\nYear: ${response.data.Year}\nIMDB Rating: ${response.data.imdbRating}\nRotten Tomatoes Rating: ${response.data.Ratings[1].Value}\nCountry: ${response.data.Country}\nLanguage: ${response.dataLanguage}\nPlot: ${response.data.Plot}\nActors: ${response.data.Actors}\n\n`);
         })
         .catch(function (error) {
             console.log(error);
         })
-    console.log("This is the OMDB search");
 }
 
 function concertSearch() {
-    console.log(`\n\nUpcoming concerts for ${display}\n\n`);
+    console.log(`\n\n========== UPCOMING CONCERTS FOR: ${display} ==========\n\n`);
     axios.get("https://rest.bandsintown.com/artists/" + searchConcert + "/events?app_id=codingbootcamp").then(function (response) {
-     for (var i = 0; i < response.data.length; i++) {
-         console.log(`Venue: ${response.data[i].venue.name}\nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}\nDate: ${moment}(${response.data[i].datetime}).${format(MM/DD/YYYY)}\n\n`);
-     };
+        for (var i = 0; i < response.data.length; i++) {
+            var concertDate = moment(response.data[i].datetime).format("MM/DD/YYYY");
+            // console.log(concertDate);
+            console.log(`--------------------\nVenue: ${response.data[i].venue.name}\nLocation: ${response.data[i].venue.city}, ${response.data[i].venue.region}, ${response.data[i].venue.country}\nDate: ${concertDate}\n--------------------\n\n`);
+        };
     });
-  };
-function doThis() {
-    console.log("This is Do This");
+};
+
+function doWhatItSays() {
+
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        var dataArray = data.split(",");
+        action = dataArray[0].toLowerCase();
+        searchTerm = dataArray[1].replace(/"/g, '').toLowerCase();
+        searchMovie = searchTerm.replace(/ /g, "+");
+        searchConcert = searchTerm.replace(/ /g, "%20");
+        display = dataArray[1].replace(/"/g, '');
+
+        switch (action) {
+            case "spotify-this-song":
+                spotifySearch();
+                break;
+            case "movie-this":
+                omdbSearch();
+                break;
+            case "concert-this":
+                concertSearch();
+                break;
+            default:
+                return console.log("Please input a proper command.");
+        }
+    })
 }
+
+
 
 switch (action) {
     case "concert-this":
@@ -74,8 +104,8 @@ switch (action) {
         omdbSearch();
         break;
     case "do-what-it-says":
-        doThis();
+        doWhatItSays();
         break;
     default:
-        console.log("Please input proper command.")
+        console.log("Please input a proper command.")
 }
